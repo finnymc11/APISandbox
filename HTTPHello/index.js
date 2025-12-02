edffb871658064678445097f12cb88352d6c8c12
Finbar McCarron <115332264+finnymc11@users.noreply.github.com>
2025-12-01 21:00:24 -0500

    Update index.js

--- HTTPHello/index.js
+++ HTTPHello/index.js
@@ -1,14 +1,14 @@
 module.exports = async function (context, req) {
+  context.log("HTTPHello function processed a request.");
+
   const name = req.query.name || (req.body && req.body.name);
 
-  const responseMessage = name
-    ? `Hello, ${name}. This HTTP triggered function executed successfully. Hi, ${name}!`
+  const message = name
+    ? `Hello, ${name}. This HTTP triggered function executed successfully.`
     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-  context.log("HTTP trigger processed a request.");
 
   context.res = {
     status: 200,
-    body: responseMessage
+    body: message
   };
 };
